2d9f48a3dd385ab31e0556a0e73049a48720dadd
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-10-27 19:59:43 -0300

    27/10/21 19:58

--- portfolio/portfolio.js
+++ portfolio/portfolio.js
@@ -2,21 +2,19 @@ function addServiceCard (params){
     console.log(params);
 
     const template = document.querySelector("#template-card");
-    const contentCard = document.querySelector(".mis-servicios");
+    const contentCard = document.querySelector(".portfolio__conteiner");
 
     template.content.querySelector(".mis-servicios__titulo").textContent = params.title;
     template.content.querySelector(".mis-servicios__parrafo").textContent = params.description;
-    template.content.querySelector(".mis-servicios__card__img");  
+    template.content.querySelector(".mis-servicios__card__img").src = params.img;  
 
     const clone = document.importNode(template.content, true);
     contentCard.appendChild(clone);
 }
 
 
-
-
 function getServices(){
-    return fetch('https://cdn.contentful.com/spaces/dxb7qs3xep4m/environments/master/entries?access_token=r3MKPI7nT3W0wQiibTHSZ6yOwnLnqY59BqiemAhzuiE&content_type=servicios')
+    return fetch('https://cdn.contentful.com/spaces/dxb7qs3xep4m/environments/master/entries?access_token=r3MKPI7nT3W0wQiibTHSZ6yOwnLnqY59BqiemAhzuiE&content_type=portfolio')
     .then(res=>{
         return res.json();
     }).then((json)=>{
@@ -24,15 +22,13 @@ function getServices(){
             return {
                 title: item.fields.title,
                 description: item.fields.description,
-
+                img: item.fields.urlImg
             }
         })
         console.log(json);
         return fieldsCollection;
     });
 }
-
-
 
 
 function main (){
