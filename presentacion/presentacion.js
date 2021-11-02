0357748d4b3e420135552366ab14eb5a4a8fc535
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-11-02 13:16:30 -0300

    2/11/2021 13:16

--- presentacion/presentacion.js
+++ presentacion/presentacion.js
@@ -8,7 +8,7 @@ function addElement(el) {
         <p class="presentacion__texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil minus obcaecati delectus ratione doloremque dolores quis voluptas hic voluptatem placeat nobis veritatis ex odio officiis illum, unde magni enim aperiam.</p>
     </div>
     <div class="presentacion__img-conteiner">
-        <img class="presentacion__img" src="/img/image5.png" alt="">
+        <img class="presentacion__img" src="./img/image5.png" alt="">
     </div>
 </div>`;
     el.appendChild(element);
