331a25713ab3994c7a6abe0ead500940a8b17c0a
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-11-10 20:16:17 -0300

    10/11/2021 20:16

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,14 +2,16 @@ function footerComponent (el){
     const headerEL = document.createElement("div");
 
     headerEL.innerHTML = `
-    <div class="footer__img-conteiner">
-        <a href=""><img class="bienvenida__header__img" src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/Logo-Gonzalo.png?raw=true"></a>
-    </div>
-    <div class="footer__img-conteiner--redes">
-        <a href="https://www.instagram.com/gonzalo.rythmic/?hl=es"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/InstagramIcono.png?raw=true"></a>
-        <a href="https://www.linkedin.com/in/gonzalo-nahuel-cortez-salazar-346a3a138/"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/LinkedinIcono.png?raw=true"></a>
-        <a href="https://github.com/GonzaloRythmic?tab=repositories"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/GitHubIcono.png?raw=true"></a>
+    <div class="footer-conteiner>
+        <div class="footer__img-conteiner">
+            <a href=""><img class="bienvenida__header__img" src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/Logo-Gonzalo.png?raw=true"></a>
+        </div>
+        <div class="footer__img-conteiner--redes">
+            <a href="https://www.instagram.com/gonzalo.rythmic/?hl=es"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/InstagramIcono.png?raw=true"></a>
+            <a href="https://www.linkedin.com/in/gonzalo-nahuel-cortez-salazar-346a3a138/"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/LinkedinIcono.png?raw=true"></a>
+            <a href="https://github.com/GonzaloRythmic?tab=repositories"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/GitHubIcono.png?raw=true"></a>
+        </div>
     </div>`;
-
+    
     el.appendChild(headerEL);
 }
--- index.js
+++ index.js
@@ -1,3 +1,55 @@
+
+// function addElement(el) {
+//     const element = document.createElement("div");
+  
+//     element.innerHTML = `
+//         <div class="mis-servicios__conteiner-full">
+//             <div class="mis-servicios__conteiner">
+//                 <div class="mis-servicios__card">
+//                     <img class="mis-servicios__card__img" src="" alt="">
+//                     <h3 class="mis-servicios__titulo"></h3>
+//                     <p class="mis-servicios__parrafo"></p>
+//                 </div>
+//             </div>
+//         </div>`;
+//     el.appendChild(element);
+// }
+
+function addServiceCard (params){
+    console.log(params);
+
+    const template = document.querySelector("#template-card");
+    const contentCard = document.querySelector(".servicio__conteiner");
+
+    template.content.querySelector(".mis-servicios__titulo").textContent = params.title;
+    template.content.querySelector(".mis-servicios__parrafo").textContent = params.description;
+    template.content.querySelector(".mis-servicios__card__img").src = params.img;  
+    
+
+    const clone = document.importNode(template.content, true);
+    contentCard.appendChild(clone);
+}
+
+
+function getServices(){
+    return fetch('https://cdn.contentful.com/spaces/dxb7qs3xep4m/environments/master/entries?access_token=r3MKPI7nT3W0wQiibTHSZ6yOwnLnqY59BqiemAhzuiE&content_type=services')
+    .then(res=>{
+        return res.json();
+    }).then((json)=>{
+        const fieldsCollection = json.items.map((item)=>{
+            return {
+                title: item.fields.title,
+                description: item.fields.description,
+                img: item.fields.urlImg,
+                url: item.fields.url
+            }
+        })
+        console.log(json);
+        return fieldsCollection;
+    });
+}
+
+
 function main () {
     //Apendeamos los elementos.
     //header
@@ -10,7 +62,7 @@ function main () {
     footerComponent (document.querySelector(".footer"));
     
     //JS del burguerMenu
-    burguerMenu();
+    burguerMenu();   
 
     addElement(document.querySelector("#template-card"))
     getServices().then(function (services) {
