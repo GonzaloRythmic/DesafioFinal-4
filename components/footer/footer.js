bf50acdb08ee81eba0aaf25cb4d00c94a4f274e4
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-11-10 20:45:13 -0300

    10/11/2021 20:45

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -2,7 +2,6 @@ function footerComponent (el){
     const headerEL = document.createElement("div");
 
     headerEL.innerHTML = `
-    <div class="footer-conteiner>
         <div class="footer__img-conteiner">
             <a href=""><img class="bienvenida__header__img" src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/Logo-Gonzalo.png?raw=true"></a>
         </div>
@@ -10,8 +9,7 @@ function footerComponent (el){
             <a href="https://www.instagram.com/gonzalo.rythmic/?hl=es"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/InstagramIcono.png?raw=true"></a>
             <a href="https://www.linkedin.com/in/gonzalo-nahuel-cortez-salazar-346a3a138/"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/LinkedinIcono.png?raw=true"></a>
             <a href="https://github.com/GonzaloRythmic?tab=repositories"><img src="https://github.com/GonzaloRythmic/DesafioFinal-4/blob/master/img/GitHubIcono.png?raw=true"></a>
-        </div>
-    </div>`;
-    
+        </div>`;
+    headerEL.className = "footer-conteiner"
     el.appendChild(headerEL);
 }
