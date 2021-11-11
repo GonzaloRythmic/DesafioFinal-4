cdd8a617a30bef93e5b52a8f652c2caa26e22053
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-11-10 21:08:10 -0300

    10/11/2021 21:08

--- components/header/header.js
+++ components/header/header.js
@@ -23,8 +23,8 @@ function headerComponent (el){
             </div>
             <div class="header__options-teamplate">
                 <a href="./portfolio/portfolio.html">Portfolio</a>
-                   <a href="/servicios/servicios.html">Servicios</a>
-                   <a href="/contacto/contacto.html">Contacto</a>   
+                   <a href="./servicios/servicios.html">Servicios</a>
+                   <a href="./contacto/contacto.html">Contacto</a>   
             </div>
         </header>
         `;
