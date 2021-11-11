c72e257ae98a6e22f7121f079d6e72f5307999db
GonzaloRythmic <gonzalocortez1991@hotmail.com>
2021-11-11 19:30:02 -0300

    11/11/2021 19:29

--- components/header/header.js
+++ components/header/header.js
@@ -8,9 +8,9 @@ function headerComponent (el){
             </div>
             <div class="options">
                 <h2 class="options__text">
-                    <a href="../portfolio/portfolio.html">Portfolio</a>
-                    <a href="../servicios/servicios.html">Servicios</a>
-                    <a href="../contacto/contacto.html">Contacto</a>  
+                    <a href="./portfolio/portfolio.html">Portfolio</a>
+                    <a href="./servicios/servicios.html">Servicios</a>
+                    <a href="./contacto/contacto.html">Contacto</a>  
                 </h2>
             </div>
         </div>
